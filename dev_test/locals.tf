f615bef9db112917fd173d7b5e8948dc47058c4d
Anatoli_Padlipski <ohlol89@gmail.com>
2021-01-13 14:41:08 +0300

    nice

--- dev_test/locals.tf
+++ dev_test/locals.tf
@@ -1,7 +1,7 @@
 locals {
   counter                       = 2
 
-  group_name                    = "webservers"
+  group_name                    = "web-servers"
   location                      = "uksouth"
   name                          = "slave"
   size                          = "Standard_B1s"
@@ -13,14 +13,14 @@ locals {
   offer                         = "UbuntuServer"
   sku                           = "18.04-LTS"
   version                       = "latest"
-  network_name                  = "webservers-network"
+  network_name                  = "web-servers-network"
   address_space                 = ["10.0.0.0/16"]
   subnet_name                   = "internal"
   address_prefixes              = ["10.0.2.0/24"]
   interface_name                = "network_interface"
   private_ip_address_allocation = "Dynamic"
 
-  public_IP_name               = "slavePublicIP"
+  public_IP_name               = "slave-PublicIP"
   public_IP_location           = "uksouth"
   allocation_method            = "Dynamic"
 
